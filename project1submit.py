d9f7ed9d31f7f2594a4a294003d58cb515021992
VibhaDhar <dharvibha@gmail.com>
2016-05-17 00:41:02 -0500

    Update project1submit.py

--- project1submit.py
+++ project1submit.py
@@ -2,7 +2,6 @@
    Python program runs on local host, uploads, downloads, encrypts local files to google.
    Please use python 2.7.X, pycrypto 2.6.1 and Google Cloud python module '''
 
-#import statements.
 import argparse
 import httplib2
 import os
